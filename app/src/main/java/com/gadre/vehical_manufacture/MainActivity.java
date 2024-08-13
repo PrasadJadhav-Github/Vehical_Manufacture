8e75eacee6aea0e3790864d89dcf1318b332fb50
prasad <prasadrjadhav11@gmail.com>
2024-08-13 11:38:47 +0530

    singleton done with display data

--- app/src/main/java/com/gadre/vehical_manufacture/MainActivity.java
+++ app/src/main/java/com/gadre/vehical_manufacture/MainActivity.java
@@ -1,20 +1,43 @@
 package com.gadre.vehical_manufacture;
 
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import com.gadre.vehical_manufacture.data.CarInfoDataClass;
+import com.gadre.vehical_manufacture.data.Manufacturer;
+import com.gadre.vehical_manufacture.data.VehicalType;
+import com.gadre.vehical_manufacture.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        ManufacturerProvider.getInstance().getCarInfoData();
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        // Get the singleton instance of ManufacturerProvider
+        ManufacturerProvider provider = ManufacturerProvider.getInstance();
+
+        // Retrieve car info data
+        CarInfoDataClass carInfoData = provider.getCarInfoData();
+
+        // Display the details of each manufacturer
+        String cardetails = "";
+        //Iterates over each Manufacturer object in the list returned by carInfoData.getResults()
+        for (Manufacturer manufacturer : carInfoData.getResults()) {
+            cardetails += "Country: " + manufacturer.getCountryName() + "\n"
+                    + "Name: " + manufacturer.getMrfName() + "\n"
+                    + "ID: " + manufacturer.getMrfID() + "\n"
+                    + "Full Name: " + manufacturer.getMrfCommonName() + "\n"
+                    + "Vehicle Types: ";
+            for (VehicalType vehicalType : manufacturer.getVehicalTypes()) {
+                cardetails += vehicalType.getName() + " ";
+            }
+            cardetails += "\n\n";
+        }
+        binding.textViewDetails.setText(cardetails);
 
     }
 }
